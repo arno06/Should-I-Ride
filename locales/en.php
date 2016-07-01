aa4fedd31baa4726c772980898dda4de8fd26250
arno06 <arno06@gmail.com>
2016-07-01 18:27:14 +0200

    Initial Commit

---
+++ locales/en.php
@@ -0,0 +1,27 @@
+<?php
+
+$dictionary = [
+    'days'=>[
+        'Mon',
+        'Tue',
+        'Wen',
+        'Thu',
+        'Fri',
+        'Sat',
+        'Sun'
+    ],
+    'months'=>[
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aou',
+        'Sept',
+        'Oct',
+        'Nov',
+        'Dec'
+    ]
+];
---
+++ locales/fr.php
@@ -0,0 +1,27 @@
+<?php
+
+$dictionary = [
+    'days'=>[
+        'Lun',
+        'Mar',
+        'Mer',
+        'Jeu',
+        'Ven',
+        'Sam',
+        'Dim'
+    ],
+    'months'=>[
+        'Jan',
+        'Fev',
+        'Mar',
+        'Arv',
+        'Mai',
+        'Juin',
+        'Juil',
+        'Aou',
+        'Sept',
+        'Oct',
+        'Nov',
+        'Dec'
+    ]
+];
